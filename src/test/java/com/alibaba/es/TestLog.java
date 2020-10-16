ae1dbe6d23cc369e850f291c1a2c4d1fe6d5a297
wb-wsc730020 <wb-wsc730020@alibaba-inc.com>
2020-10-16 16:57:35 +0800

    新增模拟日志生成程序

---
+++ src/test/java/com/alibaba/es/TestLog.java
@@ -0,0 +1,31 @@
+package com.alibaba.es;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import java.util.Random;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class TestLog {
+
+    private static final Logger LOGGER= LoggerFactory.getLogger(TestLog.class);
+
+    @Test
+    public void testLog(){
+        Random random =new Random();
+
+        while (true){
+            int userid=random.nextInt(10);
+            LOGGER.info("userId:{},send:{}",userid,"hello world.I am "+userid);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
